3f12609f6160a7ecfad666a5f18911bf26f5f5be
socar-chungmyoung <chungmyoung@socar.kr>
2022-11-09 17:46:36 +0900

    init

---
+++ src/main/kotlin/com/example/matzipbot/MatzipbotApplication.kt
@@ -0,0 +1,11 @@
+package com.example.matzipbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatzipbotApplication
+
+fun main(args: Array<String>) {
+	runApplication<MatzipbotApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/matzipbot/MatzipbotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.matzipbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MatzipbotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
